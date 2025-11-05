b2207f1de9c1ba883b08a857b483dc21989a2373
HichamAl <h.almakroudi@icloud.com>
2025-11-05 22:14:53 +0100

    add the createplayer factory function

--- main.js
+++ main.js
@@ -3,4 +3,22 @@ const gameboardObject = (function createGameboardObject(){
     return { gameboard }
 })();
 
-console.log(gameboardObject.gameboard);
+console.log(`This is the gameboard: ${gameboardObject.gameboard}`);
+
+function createPlayer(name, icon){
+    return { name , icon }
+}
+
+const user1 = createPlayer('hicham', 'X');
+const user2 = createPlayer('amine', 'O');
+console.log(`All players: Player1: ${user1.name} with icon ${user1.icon}, Player2: ${user2.name} with icon ${user2.icon}.`);
+
+
+function game(user1, user2){
+    // deze functie beheert de flow van de game
+
+    // als een van de 8 manieren om te winnen in boter kaas en eieren dan einde ronde
+
+    // of gelijkspel, als niemand heeft gewonnen en er geen plekken meer over zijn 
+
+}
