413b4d0a228c1be612dcffa597915df80c9ff87a
HichamAl <104017860+HichamAl@users.noreply.github.com>
2025-11-16 20:10:04 +0100

    added a factory function that returns an object which contains the players name, move they made and their marker which is then added to the gameboard allowing me to see who made what move

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 const gameboardObject = (function createGameboardObject(){
-    const gameboard = ['X','X','X','O','X','X','X','O','O'];
+    const gameboard = [];
     return { gameboard }
 })();
 
-console.log(`This is the gameboard: ${gameboardObject.gameboard}`);
-
-function createPlayer(name, icon){
-    return { name , icon }
+function createPlayer(name, marker){
+    return { name , marker }
 }
 
-const user1 = createPlayer('hicham', 'X');
-const user2 = createPlayer('amine', 'O');
-console.log(`All players: Player1: ${user1.name} with icon ${user1.icon}, Player2: ${user2.name} with icon ${user2.icon}.`);
+const playerOne = createPlayer('hicham', 'X');
+const PlayerTwo = createPlayer('amine', 'O');
 
-
-function game(user1, user2){
-    // deze functie beheert de flow van de game
-
-    // als een van de 8 manieren om te winnen in boter kaas en eieren dan einde ronde
-
-    // of gelijkspel, als niemand heeft gewonnen en er geen plekken meer over zijn 
-
+function gameFlowController(){
 }
+
+function createInputObject(input,playername, marker){
+    let playerName = playername;
+    let Marker = marker;
+    let choice = input;
+    return { playerName, Marker, choice }
+}
+
+let PlayerInput = 'topleft';
+let PlayerInputObject = createInputObject(PlayerInput, playerOne.name, playerOne.marker);
+gameboardObject.gameboard.push(PlayerInputObject);
+
+PlayerInput = 'topleft';
+// check if a move was already done on the gameboard
+if (gameboardObject.gameboard.some(e => e.choice === PlayerInput) ){
+    console.log(`${PlayerInput} was found in the gameboard, which means this move was already done.`);
+    PlayerInput = 'topright';
+    PlayerInputObject = createInputObject(PlayerInput, PlayerTwo.name, PlayerTwo.marker);
+    gameboardObject.gameboard.push(PlayerInputObject);
+} else {
+    PlayerInputObject = createInputObject(PlayerInput, PlayerTwo.name, PlayerTwo.marker);
+    gameboardObject.gameboard.push(PlayerInputObject);
+}
+
+
